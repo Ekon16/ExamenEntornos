087f8bf6b2ce4c14f878ad3a6984ab9a3c5e0a0e
Ekon16 <jigodino11@gmail.com>
2022-06-01 12:10:55 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -11,9 +11,17 @@ package Cheese;
  * @author ___
  */
 public class Main {
-    
+    ListaProductos produc = new ListaProductos();
      public static void main(String[] args) {
     
          System.out.println("1º Gdam: Entornos de desarrollo");
+         
+         
+        String cod1 = "0001";        
+        Producto prod1 = new Producto(cod1, "vaca", 2, 1.2);
+         
+        String cod2 = "0002";        
+        Producto prod2 = new Producto(cod2, "cabra", 3, 1.2);
+         
     }
 }
